7149bc93e29ca612c9b4b457e6632aa8f1c19e88
divakaivan <timee98@outlook.com>
2026-06-17 20:36:58 +0100

    test happy path

--- tests/unit_tests/test__main.py
+++ tests/unit_tests/test__main.py
@@ -1,15 +1,42 @@
 import pytest
+from fastapi import status
 from fastapi.testclient import TestClient
 from src.files_api.main import APP
 
 
 @pytest.fixture
-def client(mocked_aws) -> TestClient:  # pylint: disable=unused-argument
+def client(mocked_aws) -> TestClient:
     with TestClient(APP) as client:
         yield client
 
 
-def test_upload_file(client: TestClient): ...
+def test_upload_file(client: TestClient):
+    test_file_path = "some/nested/file.txt"
+    test_file_content = b"some content"
+    test_file_content_type = "text/plain"
+
+    response = client.put(
+        f"/files/{test_file_path}",
+        files={"file": (test_file_path, test_file_content, test_file_content_type)},
+    )
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.json() == {
+        "file_path": test_file_path,
+        "message": f"New file uploaded at path: /{test_file_path}",
+    }
+
+    updated_content = b"updated content"
+    response = client.put(
+        f"/files/{test_file_path}",
+        files={"file": (test_file_path, updated_content, test_file_content_type)},
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "file_path": test_file_path,
+        "message": f"Existing file updated at path: /{test_file_path}",
+    }
 
 
 def test_list_files_with_pagination(client: TestClient): ...
