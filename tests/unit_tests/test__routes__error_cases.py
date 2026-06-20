9a93e7f51c37ff6b27dd2c07334802cec0daa971
divakaivan <timee98@outlook.com>
2026-06-20 18:03:20 +0100

    test page size error case 422

--- tests/unit_tests/test__routes__error_cases.py
+++ tests/unit_tests/test__routes__error_cases.py
@@ -1,3 +1,5 @@
+from src.files_api.schemas import DEFAULT_GET_FILES_MAX_PAGE_SIZE
+from src.files_api.schemas import DEFAULT_GET_FILES_MIN_PAGE_SIZE
 from fastapi import status
 from fastapi.testclient import TestClient
 
@@ -17,3 +19,11 @@ def test_delete_nonexistent_file(client: TestClient):
     response = client.delete("/files/nonexistent_file.txt")
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {"detail": "File not found"}
+
+
+def test_get_files_invalid_page_size(client: TestClient):
+    response = client.get(f"/files?page_size={DEFAULT_GET_FILES_MIN_PAGE_SIZE - 1}")
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
+
+    response = client.get(f"/files?page_size={DEFAULT_GET_FILES_MAX_PAGE_SIZE + 1}")
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
