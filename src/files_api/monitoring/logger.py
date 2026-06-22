ef8e9402a64bc090e284ecf7052f97d5ef4c2d18
divakaivan <timee98@outlook.com>
2026-06-22 23:26:58 +0100

    make tracebacks look nicer

--- src/files_api/monitoring/logger.py
+++ src/files_api/monitoring/logger.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+import traceback
 
 from fastapi import Request, Response
 from loguru import logger
@@ -12,7 +13,7 @@ def configure_logger():
         sink=sys.stdout,
         level=os.getenv("LOGURU_LEVEL", "DEBUG"),
         diagnose=False,
-        format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | <bold><white>{message}</white></bold> | <dim>{extra}</dim>",
+        format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | <bold><white>{message}</white></bold> | <dim>{extra}</dim> {stacktrace}",
         filter=process_log_record,
     )
 
@@ -22,7 +23,29 @@ def process_log_record(record):
     if extra:
         record["extra"] = json.dumps(extra, default=str)
 
-    return True
+    record["stacktrace"] = ""
+    if record["exception"]:
+        err = record["exception"]
+        stacktrace = get_formatted_stacktrace(
+            err, replace_newline_character_with_carriage_return=True
+        )
+        record["stacktrace"] = stacktrace
+
+    return record
+
+
+def get_formatted_stacktrace(
+    loguru_record_exception, replace_newline_character_with_carriage_return: bool
+) -> str:
+    """Get the formatted stacktrace for the current exception."""
+    exc_type, exc_value, exc_traceback = loguru_record_exception
+    stacktrace_: list[str] = traceback.format_exception(
+        exc_type, exc_value, exc_traceback
+    )
+    stacktrace: str = "".join(stacktrace_)
+    if replace_newline_character_with_carriage_return:
+        stacktrace = stacktrace.replace("\n", "\r")
+    return stacktrace
 
 
 def log_request_info(request: Request):
